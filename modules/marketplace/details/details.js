bb8187e20f9e0673b884e7983c9c70f81c51b711
Julien Dubois <julien.dubois@gmail.com>
2015-12-06 22:52:58 -0500

    details now display correct download stats

--- modules/marketplace/details/details.js
+++ modules/marketplace/details/details.js
@@ -6,18 +6,48 @@ angular.module('marketplace.details', ['ngRoute'])
   });
 }])
 
-marketplaceApp.controller('ModuleDetailsCtrl', function ($scope, $routeParams, $http) {
-    $scope.npmPackageName = $routeParams.npmPackageName;
+marketplaceApp.controller('ModuleDetailsCtrl', function ($scope, $routeParams, $http, $location) {
+  $scope.npmPackageName = $routeParams.npmPackageName;
 
-    $http.get('https://api.npmjs.org/downloads/range/last-month/generator-jhipster-fortune').success(function(data) {
-      $scope.downloads = data.downloads;
+  $http.get('marketplace/data/modules.json').success(function(data) {
+      for (var i = 0; i < data.length; i++) {
+          var module = data[i];
+          if (module.npmPackageName == $scope.npmPackageName) {
+              $scope.module = module;
+          }
+      }
+    });
+
+  $http.get('https://api.npmjs.org/downloads/range/last-month/' + $scope.npmPackageName).success(function(data) {
+      var dates = ['date'];
+      var downloads = ['downloads'];
+      for (var i = 0; i < data.downloads.length; i++) {
+          var download = data.downloads[i];
+          dates.push(download.day);
+          downloads.push(download.downloads);
+      }
       var chart = c3.generate({
         bindto: '#chart',
         data: {
-        columns: [
-          ['data1', 30, 200, 100, 400, 150, 250]
-        ]
-    }
-});
+          x : 'date',
+          xFormat : '%Y-%m-%d',
+          columns: [
+            dates,
+            downloads
+          ]
+        },
+        axis : {
+          x : {
+            type : 'timeseries',
+            tick : {
+              format : "%e %b %y" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
+            }
+          }
+        }
+      });
     });
+
+  $scope.list = function() {
+    $location.path('/list');
+  };
 });
--- modules/marketplace/list/list.js
+++ modules/marketplace/list/list.js
@@ -9,6 +9,13 @@ angular.module('marketplace.list', ['ngRoute'])
 marketplaceApp.controller('ModuleListCtrl', function ($scope, $http, $location) {
   $http.get('marketplace/data/modules.json').success(function(data) {
     $scope.modules = data;
+    var modulesList= '';
+    for (var i = 0; i < data.length; i++) {
+        modulesList += data[i].npmPackageName + ',';
+    }
+    $http.get('https://api.npmjs.org/downloads/point/last-month/' + modulesList).success(function(data) {
+        // TODO print download stats
+    });
   });
 
   $scope.details = function(npmPackageName) {
